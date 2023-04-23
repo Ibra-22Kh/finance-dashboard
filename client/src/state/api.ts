efc187168bbed36fd70059c667862a9fbd3b9248
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-23 17:15:29 +0300

    added the transaction api and model to api then  types

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,23 +1,28 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse, GetProductsResponse, GetTransactionsResponse } from "./types";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  GetKpisResponse,
+  GetProductsResponse,
+  GetTransactionsResponse,
+} from "./types";
+
 export const api = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}),
-    reducerPath: "main",
-    tagTypes: ["Kpis","Products","Transactions"],
-    endpoints: (build) =>({
-        getKpis: build.query<Array<GetKpisResponse>, void>({
-            query:()=> "kpi/kpis/",
-            providesTags: ["Kpis"],
-        }),
-        getProducts: build.query<Array<GetProductsResponse>, void>({
-            query:()=> "product/products/",
-            providesTags: ["Products"],
-        }),
-        getTransactinos: build.query<Array<GetTransactionsResponse>, void>({
-            query:()=> "transaction/transactions/",
-            providesTags: ["Transactions"],
-        }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: ["Kpis", "Products", "Transactions"],
+  endpoints: (build) => ({
+    getKpis: build.query<Array<GetKpisResponse>, void>({
+      query: () => "kpi/kpis/",
+      providesTags: ["Kpis"],
     }),
+    getProducts: build.query<Array<GetProductsResponse>, void>({
+      query: () => "product/products/",
+      providesTags: ["Products"],
+    }),
+    getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+      query: () => "transaction/transactions/",
+      providesTags: ["Transactions"],
+    }),
+  }),
 });
 
-export const {useGetKpisQuery, useGetProductsQuery ,useGetTransactinosQuery} = api;
+export const { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } = api;
--- client/src/state/types.ts
+++ client/src/state/types.ts
@@ -47,14 +47,13 @@ export interface GetProductsResponse{
     udpatedAt:string;
 }
 
-export interface GetTransactionsResponse{
+export interface GetTransactionsResponse {
     id: string;
-    _id:string;
-    __v:number;
-    buyer:string;
-    price:number;
-    amount:number;
-    productIds:Array<string>;
-    createdAt:string;
-    udpatedAt:string;
+    _id: string;
+    __v: number;
+    buyer: string;
+    amount: number;
+    productIds: Array<string>;
+    createdAt: string;
+    updatedAt: string;
 }
