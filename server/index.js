9b49c2b1c438f9a1013e1d91f81267e1d43c1529
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-23 12:24:35 +0300

    index for pushing the data to DB updated

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import kpiRoutes from './routes/kpi.js';
+import productRoutes from './routes/product.js';
+import Product from './models/Product.js'
+import KPI from "./models/KPI.js";
+import {kpis,products} from "./data/data.js"
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -16,6 +21,10 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+/* ROUTES */
+app.use("/kpi",kpiRoutes);
+app.use("/product",productRoutes);
 
 /* MONOGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -30,7 +39,7 @@ mongoose
     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
-    // Product.insertMany(products);
+    //Product.insertMany(products);
     // Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
